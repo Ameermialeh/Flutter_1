af292a948c7a691b06404a17144d202696a8d453
Ameer mialeh <113940607+Ameermialeh@users.noreply.github.com>
2024-02-08 15:58:19 +0200

    Add files via upload

---
+++ constants/city.dart
@@ -0,0 +1,34 @@
+final List<String> city = [
+  "Nablus",
+  "Ramallah",
+  "Jenin",
+  "Tulkarem",
+  "Jerusalem",
+  "Bethlehem",
+  "Hebron",
+  "Jericho",
+  "Qalqilya",
+  "Tubas",
+  "Tulkarm",
+  "Salfit"
+];
+
+final List<String> service = [
+  "Singer",
+  "DJ",
+  "Studio",
+  "Decorating",
+  "Chair rental",
+  "Stage rental",
+  "Restaurant",
+  "Organizer"
+];
+final List<String> days = [
+  "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+];
---
+++ constants/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFD784F);
+const kPrimaryLight = Color(0xFFF1E6FF);
+const kErrorColor = Color(0xFFF03738);
+
+const Color bluishClr = Color.fromARGB(255, 100, 111, 232);
+const Color pinkClr = Color.fromARGB(255, 245, 116, 77);
